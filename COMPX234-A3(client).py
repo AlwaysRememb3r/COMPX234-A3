721ca6e809dcb89b208b0cbce330d4f9d72dfcce
AlwaysRememb3r <1905718171@qq.com>
2025-05-06 18:56:09 +0800

    Update COMPX234-A3(client).py
    1.process request function
    This function creates a TCP socket and connects to the specified server, then reads requests from the file, processes each request, constructs the request message and sends it to the server, and finally receives the server's response and prints it
    2.main function
    A main function is defined as an entry point to the client program, which is responsible for checking the correctness of the command-line arguments and calling the process_requests function to handle the client request

--- COMPX234-A3(client).py
+++ COMPX234-A3(client).py
@@ -1,2 +1,54 @@
 import socket
 import sys
+# Functions that handle client requests
+def process_requests(server_host, server_port, request_file):
+    # Create a TCP socket
+    client_socket = socket.socket(socket.AF_INET, socket. SOCK_STREAM)
+    try:
+        # Connect to the server
+        client_socket.connect((server_host, server_port))
+        with open(request_file, 'r') as file:
+          
+            for line in file:
+                line = line.strip()
+                if not line:
+                    continue
+                parts = line.split(' ')
+                command = parts[0]
+                key = parts[1]
+                value = parts[2] if len(parts) == 3 else ''
+                # Check the length of the request string
+                if len(key + ' ' + value) > 970:
+                    print(f"Error: Request size exceeds limit for {line}")
+                    continue
+                # Build the request message
+                msg = f"{str(len(line) + 3).zfill(3)}{command} {key}"
+                if command == 'P':
+                    msg += f" {value}"
+                # Send a request message
+                client_socket.send(msg.encode('utf-8'))
+                # Receive a server response
+                response = client_socket.recv(1024).decode('utf-8')
+                print(f"{line}: {response}")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        client_socket.close()
+
+
+# Main function, start the client
+def main():
+    # Check the command line parameters
+    if len(sys.argv) != 4:
+        print("Usage: python client.py <server_host> <server_port> <request_file>")
+        return
+    server_host = sys.argv[1]
+    server_port = int(sys.argv[2])
+    request_file = sys.argv[3]
+
+    process_requests(server_host, server_port, request_file)
+
+
+if __name__ == "__main__":
+    main()
+
